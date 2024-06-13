604e9b4c2231d8cdc3794be1999bd97d1c56b902
1Thiago0z <thesolotov0>
2024-06-13 16:39:57 -0300

    se arreglo unas cosas

--- js/personajes.js
+++ js/personajes.js
@@ -2,37 +2,30 @@ fetch("https://hp-api.onrender.com/api/characters")
 
 .then(response => response.json())
 
-
 .then(data => {
+    console.log(data);
 const $grid = document.querySelector(".grid");
 data.forEach(character => {
-if (character.image) {
 const card = document.createElement("div");
 card.classList.add("card");
 
 let estado;
-if (character.alive === true) {
-estado = "Vivo";
-} else {
-estado = "Muerto";
+    if (character.alive === true) {
+    estado = "Vivo";
+    } else {
+    estado = "Muerto";
 }
+
 
 card.innerHTML = `
-<img src="${character.image}" alt="${character.name}">
+${character.image ? `<img src="${character.image}" alt="${character.name}">` : ''}
 <div class="card-content">
-<h3>${character.name}</h3>
-<p><strong>Especie:</strong> ${character.species}</p>
-<p><strong>Intérprete:</strong> ${character.actor}</p>
-<p><strong>Estado:</strong> ${estado}</p>
-</div>
+    <h3>${character.name}</h3>
+    <p><strong>Especie:</strong> ${character.species}</p>
+    <p><strong>Intérprete:</strong> ${character.actor}</p>
+    <p><strong>Estado:</strong> ${estado}</p>
+    </div>
 `;
-$grid.appendChild(card);
-}
+    $grid.appendChild(card);
 });
 })
-
-
-.catch(error => {
-console.error("Algo salió mal:", error);
-});
-
